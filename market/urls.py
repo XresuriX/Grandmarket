790deaa9e062ac0f5a65e341fbdf2cc8ac518da9
XresuriX <92220727+XresuriX@users.noreply.github.com>
2022-05-19 13:47:28 -0700

    Front end has mostly been taken care of such as home page stores search market etc

--- market/urls.py
+++ market/urls.py
@@ -5,4 +5,17 @@ from . import views
 app_name = 'market'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('Grandmarket/market', views.market, name='market'),
+    path('Grandmarket/add-item', views.add, name='add-item'),
+    path('Grandmarket/cart', views.cart, name='cart'),
+    path('Grandmarket/order', views.order, name='orders'),
+    path('Grandmarket/detail', views.detail, name='detail'),
+    path('Grandmarket/profile', views.profile, name='profile'),
+    path('Grandmarket/store', views.store, name='store'),
+    path('Grandmarket/search', views.search, name='search results'),
+    path('Grandmarket/stores', views.stores, name='stores'),
+    path('Grandmarket/sign-in', views.signin, name='sign-in'),
+    path('Grandmarket/sign-up', views.signup, name='sign-up'),
+    path('Grandmarket/favorites', views.favorites, name='favorites'),
+    path('Grandmarket/about', views.about, name='about'),
 ]
--- market/views.py
+++ market/views.py
@@ -4,3 +4,59 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'market/home.html')
+
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def add(request):
+    return render(request, 'market/add.html')
+
+
+def cart(request):
+    return render(request, 'market/cart.html')
+
+
+def order(request):
+    return render(request, 'market/order.html')
+
+
+def detail(request):
+    return render(request, 'market/page-detail.html')
+
+
+def profile(request):
+    return render(request, 'market/profile.html')
+
+
+def signup(request):
+    return render(request, 'market/sign-up.html')
+
+
+def signin(request):
+    return render(request, 'market/sign-in.html')
+
+
+def store(request):
+    return render(request, 'market/store_front.html')
+
+
+def search(request):
+    return render(request, 'market/search.html')
+
+
+def stores(request):
+    return render(request, 'market/stores.html')
+
+
+def new_store(request):
+    return render(request, 'market/storeform.html')
+
+
+def favorites(request):
+    return render(request, 'market/favorites.html')
+
+
+def about(request):
+    return render(request, 'market/about.html')
